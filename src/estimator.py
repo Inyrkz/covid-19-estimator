5b2a03c8d4bb0491dd66f2f61216fef656b148a5
Ini-Abasi Bernard <55382743+Inyrkz@users.noreply.github.com>
2020-04-13 22:54:18 +0100

    add new route

--- src/estimator.py
+++ src/estimator.py
@@ -85,10 +85,86 @@ def estimator():
                 "dollarsInFlight": severeImpactDollarsInFlight
                     },
            }
-  #response = Response("", 201, mimetype='application/json')
+  
+  response = Response(json.dumps(reportedCases), status=201, mimetype='application/json')
+  return response
 
-  return jsonify(reportedCases)
 
+@app.route('/api/v1/on-covid-19/<type>', methods=['POST'])
+def header(type):
+  '''function for covid-19 estimator'''
+  data = request.get_json()
+  #Challenge 1
+  def period(type = data['periodType']):
+      '''fuction to address the periodType'''
+      actual_period = data['timeToElapse']
+      if type == "days":
+          return actual_period
+      elif type == "weeks":
+          return actual_period * 7
+      elif type == "months":
+          return actual_period * 30
+
+  reportedCases = data["reportedCases"]
+  totalHospitalBeds = data["totalHospitalBeds"]
+  timeToElapse = period()
+  avgDailyIncomeInUSD = data["region"]["avgDailyIncomeInUSD"]
+  avgDailyIncomePopulation = data["region"]["avgDailyIncomePopulation"]
+    
+  impactCurrentlyInfected = int(reportedCases * 10)
+  severeImpactCurrentlyInfected = int(reportedCases * 50)
+  
+  impactInfectionsByRequestedTime = int(impactCurrentlyInfected * (2**(timeToElapse//3)))
+  severeImpactInfectionsByRequestedTime = int(severeImpactCurrentlyInfected * (2**(timeToElapse//3)))
+  
+  #Challenge 2
+  impactSevereCasesByRequestedTime = int(15 / 100 * impactInfectionsByRequestedTime)
+  severeImpactSevereCasesByRequestedTime = int(15 / 100 * severeImpactInfectionsByRequestedTime)
+  
+  impactHospitalBedsByRequestedTime = int((35 / 100 * totalHospitalBeds) - impactSevereCasesByRequestedTime)
+  severeImpactHospitalBedsByRequestedTime = int((35 / 100 * totalHospitalBeds) - severeImpactSevereCasesByRequestedTime)
+  
+  #Challenge 3
+  impactcasesForICUByRequestedTime = int(5 / 100 * impactInfectionsByRequestedTime )
+  severeImpactcasesForICUByRequestedTime = int(5 / 100 * severeImpactInfectionsByRequestedTime)
+  
+  impactcasesForVentilatorsByRequestedTime = int(2 / 100 * impactInfectionsByRequestedTime )
+  severeImpactcasesForVentilatorsByRequestedTime = int(2 / 100 * severeImpactInfectionsByRequestedTime)
+  
+  impactDollarsInFlight = int(impactInfectionsByRequestedTime * avgDailyIncomePopulation * avgDailyIncomeInUSD * timeToElapse)
+  severeImpactDollarsInFlight = int(severeImpactInfectionsByRequestedTime * avgDailyIncomePopulation * avgDailyIncomeInUSD * timeToElapse)
+  
+  reportedCases = { "data": data,
+            "impact": {
+                "currentlyInfected": impactCurrentlyInfected,
+                "infectionsByRequestedTime":  impactInfectionsByRequestedTime,
+                "severeCasesByRequestedTime":  impactSevereCasesByRequestedTime,
+                "hospitalBedsByRequestedTime": impactHospitalBedsByRequestedTime,
+                "casesForICUByRequestedTime": impactcasesForICUByRequestedTime,
+                "casesForVentilatorsByRequestedTime": impactcasesForVentilatorsByRequestedTime,
+                "dollarsInFlight": impactDollarsInFlight
+            },
+            "severeImpact": {
+                "currentlyInfected": severeImpactCurrentlyInfected,
+                "infectionsByRequestedTime":  severeImpactInfectionsByRequestedTime,
+                "severeCasesByRequestedTime":  severeImpactSevereCasesByRequestedTime,
+                "hospitalBedsByRequestedTime": severeImpactHospitalBedsByRequestedTime,
+                "casesForICUByRequestedTime": severeImpactcasesForICUByRequestedTime,
+                "casesForVentilatorsByRequestedTime": severeImpactcasesForVentilatorsByRequestedTime,
+                "dollarsInFlight": severeImpactDollarsInFlight
+                    },
+           }
+  
+  if type == 'json' :
+    response = Response(json.dumps(reportedCases), status=201, mimetype='application/json')
+    return response
+  elif type == 'xml':
+    response = Response(json.dumps(reportedCases), status=201, mimetype='application/xml')
+    return response
+  else:
+    response = Response("invalid url", status=404, mimetype='application/json')
+    return response
+    
 if __name__ == "__main__":
     app.run(debug=True)
   
