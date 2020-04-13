7bd0bfd08f09ccd418e885080e878417244335c0
Ini-Abasi Bernard <55382743+Inyrkz@users.noreply.github.com>
2020-04-13 21:26:53 +0100

    add estimator function code

--- src/estimator.py
+++ src/estimator.py
@@ -1,2 +1,78 @@
-def estimator(data):
-  return data
+from flask import Flask, request, Response, jsonify
+import json
+
+app = Flask(__name__)
+
+#Dummy data
+
+# data = {
+#         "region": {
+#           "name": "Africa",
+#           "avgAge": 19.7,
+#           "avgDailyIncomeInUSD": 4,
+#           "avgDailyIncomePopulation": 0.73
+#         },
+#         "periodType": "days",
+#         "timeToElapse": 38,
+#         "reportedCases": 2747,
+#         "population": 92931687,
+#         "totalHospitalBeds": 678874
+# }
+
+@app.route('/api/v1/on-covid-19', methods=['POST'])
+def estimator():
+  '''function for covid-19 estimator'''
+  data = request.get_json()
+  #Challenge 1
+  reportedCases = data["reportedCases"]
+  totalHospitalBeds = data["totalHospitalBeds"]
+  timeToElapse = data["timeToElapse"]
+  avgDailyIncomeInUSD = data["region"]["avgDailyIncomeInUSD"]
+  avgDailyIncomePopulation = data["region"]["avgDailyIncomePopulation"]
+    
+  impactCurrentlyInfected = int(reportedCases * 10)
+  severeImpactCurrentlyInfected = int(reportedCases * 50)
+  
+  impactInfectionsByRequestedTime = int(impactCurrentlyInfected * (2**(timeToElapse//3)))
+  severeImpactInfectionsByRequestedTime = int(severeImpactCurrentlyInfected * (2**(timeToElapse//3)))
+  
+  #Challenge 2
+  impactSevereCasesByRequestedTime = int(15 / 100 * impactInfectionsByRequestedTime)
+  severeImpactSevereCasesByRequestedTime = int(15 / 100 * severeImpactInfectionsByRequestedTime)
+  
+  impactHospitalBedsByRequestedTime = int((35 / 100 * totalHospitalBeds) - impactSevereCasesByRequestedTime)
+  severeImpactHospitalBedsByRequestedTime = int((35 / 100 * totalHospitalBeds) - severeImpactSevereCasesByRequestedTime)
+  
+  #Challenge 3
+  impactcasesForICUByRequestedTime = int(5 / 100 * impactInfectionsByRequestedTime )
+  severeImpactcasesForICUByRequestedTime = int(5 / 100 * severeImpactInfectionsByRequestedTime)
+  
+  impactcasesForVentilatorsByRequestedTime = int(2 / 100 * impactInfectionsByRequestedTime )
+  severeImpactcasesForVentilatorsByRequestedTime = int(2 / 100 * severeImpactInfectionsByRequestedTime)
+  
+  impactDollarsInFlight = int(impactInfectionsByRequestedTime * avgDailyIncomePopulation * avgDailyIncomeInUSD * timeToElapse)
+  severeImpactDollarsInFlight = int(severeImpactInfectionsByRequestedTime * avgDailyIncomePopulation * avgDailyIncomeInUSD * timeToElapse)
+  
+  reportedCases = { "data": data,
+            "impact": {
+                "currentlyInfected": impactCurrentlyInfected,
+                "infectionsByRequestedTime":  impactInfectionsByRequestedTime,
+                "severeCasesByRequestedTime":  impactSevereCasesByRequestedTime,
+                "hospitalBedsByRequestedTime": impactHospitalBedsByRequestedTime,
+                "casesForICUByRequestedTime": impactcasesForICUByRequestedTime,
+                "casesForVentilatorsByRequestedTime": impactcasesForVentilatorsByRequestedTime,
+                "dollarsInFlight": impactDollarsInFlight
+            },
+            "severeImpact": {
+                "currentlyInfected": severeImpactCurrentlyInfected,
+                "infectionsByRequestedTime":  severeImpactInfectionsByRequestedTime,
+                "severeCasesByRequestedTime":  severeImpactSevereCasesByRequestedTime,
+                "hospitalBedsByRequestedTime": severeImpactHospitalBedsByRequestedTime,
+                "casesForICUByRequestedTime": severeImpactcasesForICUByRequestedTime,
+                "casesForVentilatorsByRequestedTime": severeImpactcasesForVentilatorsByRequestedTime,
+                "dollarsInFlight": severeImpactDollarsInFlight
+                    },
+           }
+    
+   return reportedCases
+
